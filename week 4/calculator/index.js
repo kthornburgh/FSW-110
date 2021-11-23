1e0e8414593d7d149019ac880eeb0582ade0eb65
kthornburgh <89819577+kthornburgh@users.noreply.github.com>
2021-11-22 21:35:59 -0600

    Add files via upload

---
+++ week 4/calculator/index.js
@@ -0,0 +1,6 @@
+function addition(a,b) {
+    var a = parseInt(document.getElementById('num1').value);
+    var b = parseInt(document.getElementById('num2').value);
+    var result = a + b;
+    document.getElementById('one') = result
+     }
