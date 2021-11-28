7350e20a5886aa9c51ff2b91ba2c774cfb3733bb
kthornburgh <89819577+kthornburgh@users.noreply.github.com>
2021-11-27 19:01:38 -0600

    Add files via upload

---
+++ week 5/submit_events/index.js
@@ -0,0 +1,20 @@
+var one = document.getElementById('text-input1')
+
+function onesub(){
+    alert(one.value)
+    document.getElementById('text-input1').value= ""
+}
+
+var two = document.getElementById('text-input2')
+
+function twosub(){
+    alert(two.value)
+    document.getElementById('text-input2').value= ""
+}
+
+var three = document.getElementById('text-input3')
+
+function threesub(){
+    alert(three.value)
+    document.getElementById('text-input3').value= ""
+}
